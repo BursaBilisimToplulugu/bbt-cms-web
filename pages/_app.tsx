1938dca2f5fc89dbf617672a7cc76e2ec908a2df
borabiricik <bora.bir496@gmail.com>
2023-04-05 21:01:10 +0300

    deneme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,8 @@ import createEmotionCache from 'src/createEmotionCache';
 import { SidebarProvider } from '@/modules/common/contexts/SidebarContext';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import { Provider } from 'react-redux';
+import { store } from '@/modules/core/store';
 
 const clientSideEmotionCache = createEmotionCache();
 
@@ -34,24 +36,26 @@ function TokyoApp(props: TokyoAppProps) {
   Router.events.on('routeChangeComplete', nProgress.done);
 
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Tokyo Free Black NextJS Typescript Admin Dashboard</title>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-      </Head>
-      <SidebarProvider>
-        <ThemeProvider>
-          <LocalizationProvider dateAdapter={AdapterDateFns}>
-            <CssBaseline />
-            {/* @ts-ignore */}
-            {getLayout(<Component {...pageProps} />)}
-          </LocalizationProvider>
-        </ThemeProvider>
-      </SidebarProvider>
-    </CacheProvider>
+    <Provider store={store}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Tokyo Free Black NextJS Typescript Admin Dashboard</title>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          />
+        </Head>
+        <SidebarProvider>
+          <ThemeProvider>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+              <CssBaseline />
+              {/* @ts-ignore */}
+              {getLayout(<Component {...pageProps} />)}
+            </LocalizationProvider>
+          </ThemeProvider>
+        </SidebarProvider>
+      </CacheProvider>
+    </Provider>
   );
 }
 
